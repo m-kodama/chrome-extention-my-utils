f47aeac57a71cd10b6c56b54f030ee5af824f0c9
m-kodama <tc003ssr@gmail.com>
2024-03-31 18:35:34 +0900

    feat: App.vueのimportのパスが古い形式だったため修正

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useSideMenu } from './composables/useSideMenu'
+import { useSideMenu } from '~/composables/useSideMenu'
 
 const { selectedMenuId, SideMenuId } = useSideMenu({})
 </script>
